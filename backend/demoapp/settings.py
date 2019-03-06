3c1b860b730000f911a49378ee9efd9ceba6dbcf
biuren <biuren@biuren.xyz>
2019-03-06 12:43:34 +0800

    Static files

--- backend/demoapp/settings.py
+++ backend/demoapp/settings.py
@@ -114,7 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "front"),
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
